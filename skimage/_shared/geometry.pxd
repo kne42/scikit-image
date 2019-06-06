1fec421dd2b82142386d4eaee31215f4f75ad65d
Kira Evans <contact@kne42.me>
2019-06-06 15:55:46 -0700

    modify point in poly algorithm

--- skimage/_shared/geometry.pxd
+++ skimage/_shared/geometry.pxd
@@ -1,6 +1,6 @@
 cdef unsigned char point_in_polygon(Py_ssize_t nr_verts, double *xp, double *yp,
-                                    double x, double y) nogil
+                                    double x, double y, double epsilon=*) nogil
 
 cdef void points_in_polygon(Py_ssize_t nr_verts, double *xp, double *yp,
                             Py_ssize_t nr_points, double *x, double *y,
-                            unsigned char *result) nogil
+                            unsigned char *result, double epsilon=*) nogil
--- skimage/_shared/geometry.pyx
+++ skimage/_shared/geometry.pyx
@@ -2,12 +2,21 @@
 #cython: boundscheck=False
 #cython: nonecheck=False
 #cython: wraparound=False
+from libc.math cimport fabs
 
 
-cdef inline unsigned char point_in_polygon(Py_ssize_t nr_verts, double *xp,
-                                           double *yp, double x,
-                                           double y) nogil:
-    """Test whether point lies inside a polygon.
+cdef enum:
+    OUTSIDE = 0
+    INSIDE  = 1
+    VERTEX  = 2
+    EDGE    = 3
+
+
+cdef unsigned char point_in_polygon(Py_ssize_t nr_verts, double *xp,
+                                    double *yp, double x, double y,
+                                    double epsilon=0.0000001) nogil:
+    """Classify where a point lies with respect to a polygon
+    using the ray-crossing algorithm [1]_.
 
     Parameters
     ----------
@@ -17,25 +26,79 @@ cdef inline unsigned char point_in_polygon(Py_ssize_t nr_verts, double *xp,
         Coordinates of polygon with length nr_verts.
     x, y : double
         Coordinates of point.
+    epsilon : double, optional
+        Error limit with which to check if point is a vertex.
+
+    Returns
+    -------
+    classification : unsigned char
+        Classification of the point: 0 if outside, 1 if inside, 2 if a vertex,
+        and 3 if an edge.
+
+    References
+    ----------
+    .. [1] O'Rourke (1998), "Computational Geometry in C",
+           Second Edition, Cambridge Unversity Press, Chapter 7
     """
-    cdef Py_ssize_t i
-    cdef unsigned char c = 0
-    cdef Py_ssize_t j = nr_verts - 1
+    cdef:
+        unsigned int r_cross = 0     # number of right edge/ray crossings
+        unsigned int l_cross = 0     # number of left edge/ray crossings
+
+        Py_ssize_t i                 # point index
+        Py_ssize_t j = nr_verts - 1  # previous point index
+
+        double x0, y0                # vertex when point is origin
+        double x1, y1                # previous vertex when point is origin
+    
     for i in range(nr_verts):
+        x0 = xp[i] - x
+        y0 = yp[i] - y
+
+        if fabs(x0) < epsilon and fabs(y0) < epsilon:
+            # it's a vertex
+            return VERTEX
+
+        x1 = xp[j] - x
+        y1 = yp[j] - y
+
+        intersection = (x0 * y1 - x1 * y0) / (y1 - y0)
+        
         if (
-            (((yp[i] <= y) and (y < yp[j])) or
-            ((yp[j] <= y) and (y < yp[i])))
-            and (x < (xp[j] - xp[i]) * (y - yp[i]) / (yp[j] - yp[i]) + xp[i])
+            # straddles the x-component of the ray
+            (y0 > 0) != (y1 > 0)
+            # crosses the ray if strictly positive intersection
+            and intersection  > 0
         ):
-            c = not c
+            r_cross += 1
+            
+        if (
+            # straddles the x-component of the ray when reversed
+            (y0 < 0) != (y1 < 0)
+            # crosses the ray if strictly negative intersection
+            and intersection < 0
+        ):
+            l_cross += 1
+        
         j = i
-    return c
+
+    if r_cross % 2 != l_cross % 2:
+        # on edge if left and right crossings not of same parity
+        return EDGE
+    elif r_cross % 2:
+        # inside if odd number of right crossings
+        return INSIDE
+    else:
+        # outside if even number of right crossings
+        return OUTSIDE
 
 
 cdef void points_in_polygon(Py_ssize_t nr_verts, double *xp, double *yp,
                             Py_ssize_t nr_points, double *x, double *y,
-                            unsigned char *result) nogil:
-    """Test whether points lie inside a polygon.
+                            unsigned char *result,
+                            double epsilon=0.0000001) nogil:
+    """Classify where each point lies with respect to a polygon
+    using the ray-crossing algorithm [1]_.
+
 
     Parameters
     ----------
@@ -47,9 +110,17 @@ cdef void points_in_polygon(Py_ssize_t nr_verts, double *xp, double *yp,
         Number of points to test.
     x, y : double array
         Coordinates of points.
-    result : unsigned char array
-        Test results for each point.
+    classifications : unsigned char array
+        Classifications of each point: 0 if outside, 1 if inside, 2 if a vertex,
+        and 3 if an edge.
+    epsilon : double, optional
+        Error limit with which to check if point is a vertex.
+
+    References
+    ----------
+    .. [1] O'Rourke (1998), "Computational Geometry in C",
+           Second Edition, Cambridge Unversity Press, Chapter 7
     """
     cdef Py_ssize_t n
     for n in range(nr_points):
-        result[n] = point_in_polygon(nr_verts, xp, yp, x[n], y[n])
+        result[n] = point_in_polygon(nr_verts, xp, yp, x[n], y[n], epsilon)
